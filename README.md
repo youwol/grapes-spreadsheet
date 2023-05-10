8676457b780dd21e76d8397897e6e5d4d847c1ef
jehadmelad <jd.melad@gmail.com>
2023-05-10 15:32:40 +0200

    🎉     start 0.1.0-wip

---
+++ README.md
@@ -0,0 +1,60 @@
+# @youwol/yw-spreadsheet
+
+
+
+This library is part of the hybrid cloud/local ecosystem
+[YouWol](https://platform.youwol.com/applications/@youwol/platform/latest).
+
+## Links
+
+[Online user-guide](https://l.youwol.com/doc/@youwol/yw-spreadsheet)
+
+[Developers documentation](https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/yw-spreadsheet)
+
+[Package on npm](https://www.npmjs.com/package/@youwol/yw-spreadsheet)
+
+[Source on GitHub](https://github.com/youwol/yw-spreadsheet)
+
+# Installation, Build, Test
+
+To install the required dependencies:
+
+```shell
+yarn
+```
+
+---
+
+To build for development:
+
+```shell
+yarn build:dev
+```
+
+To build for production:
+
+```shell
+yarn build:prod
+```
+
+---
+
+To run tests:
+
+```shell
+yarn test
+```
+
+Coverage can be evaluated using:
+
+```shell
+yarn test-coverage
+```
+
+---
+
+To generate code's documentation:
+
+```shell
+yarn doc
+```
---
+++ src/auto-generated.ts
@@ -0,0 +1,128 @@
+
+const runTimeDependencies = {
+    "externals": {
+        "@youwol/cdn-client": "^1.0.2",
+        "@youwol/flux-view": "^1.0.3",
+        "rxjs": "^6.5.5"
+    },
+    "includedInBundle": {}
+}
+const externals = {
+    "@youwol/cdn-client": {
+        "commonjs": "@youwol/cdn-client",
+        "commonjs2": "@youwol/cdn-client",
+        "root": "@youwol/cdn-client_APIv1"
+    },
+    "@youwol/flux-view": {
+        "commonjs": "@youwol/flux-view",
+        "commonjs2": "@youwol/flux-view",
+        "root": "@youwol/flux-view_APIv1"
+    },
+    "rxjs": {
+        "commonjs": "rxjs",
+        "commonjs2": "rxjs",
+        "root": "rxjs_APIv6"
+    }
+}
+const exportedSymbols = {
+    "@youwol/cdn-client": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/cdn-client"
+    },
+    "@youwol/flux-view": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/flux-view"
+    },
+    "rxjs": {
+        "apiKey": "6",
+        "exportedSymbol": "rxjs"
+    }
+}
+
+const mainEntry : {entryFile: string,loadDependencies:string[]} = {
+    "entryFile": "./index.ts",
+    "loadDependencies": [
+        "@youwol/cdn-client",
+        "@youwol/flux-view",
+        "rxjs"
+    ]
+}
+
+const secondaryEntries : {[k:string]:{entryFile: string, name: string, loadDependencies:string[]}}= {}
+
+const entries = {
+     '@youwol/yw-spreadsheet': './index.ts',
+    ...Object.values(secondaryEntries).reduce( (acc,e) => ({...acc, [`@youwol/yw-spreadsheet/${e.name}`]:e.entryFile}), {})
+}
+export const setup = {
+    name:'@youwol/yw-spreadsheet',
+        assetId:'QHlvdXdvbC95dy1zcHJlYWRzaGVldA==',
+    version:'0.1.0-wip',
+    shortDescription:"",
+    developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/yw-spreadsheet',
+    npmPackage:'https://www.npmjs.com/package/@youwol/yw-spreadsheet',
+    sourceGithub:'https://github.com/youwol/yw-spreadsheet',
+    userGuide:'https://l.youwol.com/doc/@youwol/yw-spreadsheet',
+    apiVersion:'01',
+    runTimeDependencies,
+    externals,
+    exportedSymbols,
+    entries,
+    secondaryEntries,
+    getDependencySymbolExported: (module:string) => {
+        return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
+    },
+
+    installMainModule: ({cdnClient, installParameters}:{
+        cdnClient:{install:(unknown) => Promise<Window>},
+        installParameters?
+    }) => {
+        const parameters = installParameters || {}
+        const scripts = parameters.scripts || []
+        const modules = [
+            ...(parameters.modules || []),
+            ...mainEntry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/yw-spreadsheet_APIv01`]
+        })
+    },
+    installAuxiliaryModule: ({name, cdnClient, installParameters}:{
+        name: string,
+        cdnClient:{install:(unknown) => Promise<Window>},
+        installParameters?
+    }) => {
+        const entry = secondaryEntries[name]
+        if(!entry){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const parameters = installParameters || {}
+        const scripts = [
+            ...(parameters.scripts || []),
+            `@youwol/yw-spreadsheet#0.1.0-wip~dist/@youwol/yw-spreadsheet/${entry.name}.js`
+        ]
+        const modules = [
+            ...(parameters.modules || []),
+            ...entry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/yw-spreadsheet/${entry.name}_APIv01`]
+        })
+    },
+    getCdnDependencies(name?: string){
+        if(name && !secondaryEntries[name]){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const deps = name ? secondaryEntries[name].loadDependencies : mainEntry.loadDependencies
+
+        return deps.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+    }
+}
