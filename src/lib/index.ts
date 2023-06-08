69e41fa127b3908f3cefbe9c65613918668cec36
jehadmelad <jd.melad@gmail.com>
2023-06-08 12:08:02 +0200

    🔨     index.ts => add getBlocks & getComponent functions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,10 @@
-export * from './hello.view'
+import { UploadFileBlock } from './blocks/upload-file.block'
+import { UploadFileViewComponent } from './cutom-view/upload-file-view-component'
+
+export function getBlocks() {
+    return [UploadFileBlock]
+}
+
+export function getComponents() {
+    return [UploadFileViewComponent]
+}
